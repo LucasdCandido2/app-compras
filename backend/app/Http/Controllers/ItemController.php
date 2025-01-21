706dc8c3e4579c8ab7c39168b16d4e5ba46ceace
LucasdCandido2 <lucasdcandido1994@gmail.com>
2025-01-21 08:26:44 -0300

    Adicionando o projeto completo

---
+++ backend/app/Http/Controllers/ItemController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Item::all());
+    }
+
+    public function store(Request $request)
+    {
+        $item = Item::create($request->all());
+        return response()->json($item, 201);
+    }
+}
+
